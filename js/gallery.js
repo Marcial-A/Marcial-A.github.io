1d887b8d372b2a71136511c45db96916ec8a4a48
Marcial <Marcialararacap@hotmail.com>
2017-02-08 00:27:59 -0800

    Performance up front page
    Slight performance upgrade on front page image. Changes to nav css and
    contact button.

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+<script>
+	<img onmouseover="getElementById('img1').src=this.src" src="img/gallery/img1.png">
+	<img onmouseover="getElementById('img2').src=this.src" src="img/gallery/img2.png">
+	<img onmouseover="getElementById('img3').src=this.src" src="img/gallery/img3.png">
+	<img onmouseover="getElementById('img4').src=this.src" src="img/gallery/img4.png">
+	<img onmouseover="getElementById('img5').src=this.src" src="img/gallery/img5.png">
+	<img onmouseover="getElementById('img6').src=this.src" src="img/gallery/img6.png">
+	<img onmouseover="getElementById('img7').src=this.src" src="img/gallery/img7.png">
+</script>
